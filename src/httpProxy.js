51352fb4bdab9064aeacb1d0617a325794f5b99b
zhengchao <chao.zheng@okcoin.com>
2020-12-16 18:35:59 +0800

    update post return value format

--- src/httpProxy.js
+++ src/httpProxy.js
@@ -15,6 +15,30 @@ class HttpProxy {
     return this.httpClient
       .request(paramsObj)
       .then(response => {
+        const cosmosCode = {
+            "1": 60001,
+            "2": 60002,
+            "3": 60003,
+            "4": 60004,
+            "5": 60005,
+            "6": 60006,
+            "7": 60007,
+            "8": 60008,
+            "9": 60009,
+            "10": 60010,
+            "11": 60011,
+            "12": 60012,
+            "13": 60013,
+            "14": 60014,
+            "15": 60015,
+            "16": 60016,
+            "17": 60017,
+            "18": 60018,
+            "19": 60019,
+            "20": 60020,
+            "21": 60021,
+            "111222": 60099
+        }
         if (method === 'get')  {
           return { result: response.data, status: response.status }
         }
@@ -26,7 +50,7 @@ class HttpProxy {
         }
         const data = response.data || null;
         if (data.code) {
-          fmtResponse.code = data.code
+          fmtResponse.code = cosmosCode[data.code] ? cosmosCode[data.code] : data.code
           fmtResponse.msg = data.raw_log || ''
           fmtResponse.detail_msg = fmtResponse.msg
         }
