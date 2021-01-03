30ab28c61ddccae8dd2ff891b23dcebef93e016a
Pierre <974741468@qq.com>
2021-01-03 11:14:29 +0800

    fix function validateSig, fix ut (#28)

--- __tests__/crypto.test.js
+++ __tests__/crypto.test.js
@@ -7,20 +7,15 @@ const privateKey = "29892b64003fc5c8c89dc795a2ae82aa84353bb4352f28707c2ed32aa101
 describe("crypto", () => {
 
   it("getAddressFromPrivateKey", () => {
-    const privateKey = crypto.generatePrivateKey()
     const address = crypto.getAddressFromPrivateKey(privateKey)
-    expect(address.length).toBe(46)
-  })
-
-  it("getAddressFromPrivateKey", () => {
-    const address = crypto.getAddressFromPrivateKey(privateKey)
-    expect(address).toBe("okexchain1g7c3nvac7mjgn2m9mqllgat8wwd3aptddw77gw")
+    expect(address).toBe("okexchain1jjvpmgwwgs99nhlje3aag0lackunqgj7xnrnwe")
+    expect(address.length).toBe(48)
   })
 
   it("getAddressFromPubKey", () => {
     const publicKey = crypto.getPubKeyHexFromPrivateKey(privateKey)
     const address = crypto.getAddressFromPubKey(publicKey)
-    expect(address).toBe("okexchain1g7c3nvac7mjgn2m9mqllgat8wwd3aptddw77gw")
+    expect(address).toBe("okexchain1jjvpmgwwgs99nhlje3aag0lackunqgj7xnrnwe")
   })
 
   it("getPrivateKeyFromKeyStore", () => {
@@ -49,7 +44,7 @@ describe("crypto", () => {
   it("sign", () => {
     const msg = "2f8b1705"
     const sig = crypto.sign(msg, privateKey).toString("hex")
-    expect(sig).toBe("733a985aa3f2ecb4c58b8cb9c97351bfb12931ae01665966e9f1d5bdcfc294017caf7f146b542319e40f3157d6820c223c42dc578c85ad8d99110f082ba7421a")
+    expect(sig).toBe("19d94b95f23ec8def89ac562bc47255fa138bec3185ca4e6ac6ee6912fd89cce095489ad872d1d54e303d6db33428095041f794d1c0c83a748b58befe6b98b8e")
   })
 
   it("validateSig", () => {
