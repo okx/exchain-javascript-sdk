9f3ea5bb0ad3974f6c4f12da46263483288e926d
zhengchao <zccst@qq.com>
2021-03-09 16:28:52 +0800

    add qrcode module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "babel-polyfill"
 import * as client from "./client"
 import * as crypto from "./crypto"
+import * as wallet from "./wallet"
 
 const { OKEXChainClient }  = client
 module.exports = OKEXChainClient
-module.exports.crypto = crypto
+module.exports.crypto = crypto;
+module.exports.wallet = wallet;
+
 
