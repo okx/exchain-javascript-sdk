92a1d85b024f0521d93b3cc59ae27539b6b34562
f <fan.ke@okg.com>
2023-01-05 16:19:16 +0800

    secp256k1wallet

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import "babel-polyfill"
 import * as client from "./client"
 import * as crypto from "./crypto"
 import * as wallet from "./wallet"
+import {OKCSecp256k1Wallet} from "./secp256k1/secp256k1wallet";
 
 const { OKEXChainClient }  = client
 module.exports = OKEXChainClient
 module.exports.crypto = crypto;
 module.exports.wallet = wallet;
+module.exports.OKCSecp256k1Wallet = OKCSecp256k1Wallet
 
 
